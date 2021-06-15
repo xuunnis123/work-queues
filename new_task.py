fd97bf6b78f06664c729640234da8aab7a7b3b79
Ji-Yang Lin <xuunnis123@gmail.com>
2021-06-15 11:41:10 +0800

    add comments

--- new_task.py
+++ new_task.py
@@ -2,19 +2,27 @@
 import pika
 import sys
 
+# 連接 RabbitMQ
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
+
+# 創建頻道
 channel = connection.channel()
+# 宣告以及創建Queue
+channel.queue_declare(queue='task_queue', durable=True) 
+#durable:一但RabbitMQ異常中斷，所有的訊息也都會跟著消失，為了確保所有的訊息的保存，我們必需在建立Queue的時候加入參數
 
-channel.queue_declare(queue='task_queue', durable=True)
-
-message = ' '.join(sys.argv[1:]) or "Hello World!"
+message = ' '.join(sys.argv[1:]) or "Hello World!" #設置參數接收外部傳入的字串
+'''
+不希望透過exchange的話就設為空字串
+routing_key則為Queue名稱
+'''
 channel.basic_publish(
-    exchange='',
-    routing_key='task_queue',
-    body=message,
+    exchange='', #簡單模式
+    routing_key='task_queue', #指定Queue
+    body=message, #傳送內容
     properties=pika.BasicProperties(
-        delivery_mode=2,  # make message persistent
+        delivery_mode=2,  # 1.非永久 ,2.永久 將訊息存在硬碟中，broker重啟後還是會存在
     ))
 print(" [x] Sent %r" % message)
 connection.close()
--- worker.py
+++ worker.py
@@ -12,12 +12,14 @@ print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body.decode())
-    time.sleep(body.count(b'.'))
+    time.sleep(body.count(b'.')) #以.作為停幾秒的依據，模擬出該工作忙碌
     print(" [x] Done")
     ch.basic_ack(delivery_tag=method.delivery_tag)
+    #加入ack，pass訊息給queue，不加的情況下完成的工作會重覆的派送
 
-
-channel.basic_qos(prefetch_count=1)
+channel.basic_qos(prefetch_count=1) #代表每一個Consumer每次只會執行一個工作，在ack給Queue之後才會再將下一任務指派給閒置的Consumer
+#告訴rabbitmq，用callback來接收訊息
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
+#開始接收訊息，Queue中有訊息才會調用callback來處理
 channel.start_consuming()
